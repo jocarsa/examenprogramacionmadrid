9a544e79ec53d12a60406fdaea1aab52da75054d
= <info@josevicentecarratala.com>
2024-02-06 18:24:11 +0100

    menu inicial

---
+++ discos.py
@@ -0,0 +1,12 @@
+def menu():
+    print("Escoge una opción:")
+    print("1.-Listar registros")
+    print("2.-Buscar registros")
+    print("3.-Insertar registros")
+    print("4.-Actualizar registros")
+    print("5.-Eliminar registros")
+    print("6.-Salir")
+    input("Escoge una opción: ")
+    menu()
+
+menu()
